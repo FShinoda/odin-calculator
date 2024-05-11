a6028e89aa334b3102cf0cd67d148d1ada1fc08f
FShinoda <fernanda.shinoda@hotmail.com>
2024-05-11 20:21:40 -0300

    feat: main function operate done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/// FUNCTIONS /////////
 function add(a, b){
     return a+b;
 };
@@ -13,3 +14,26 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 };
+
+function operate(operator, firstNumber, secondNumber){
+    switch(operator){
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+        case "x":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+        default:
+            return "cago"
+    }
+};
+
+/// MAIN /////////
+let firstNumber = 10
+    , secondNumber = 2
+    , operator = '/';
+
+console.log(operate(operator, firstNumber, secondNumber));

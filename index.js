7f0b438df8dcb2d481d9ae466642408b2dec034b
FShinoda <fernanda.shinoda@hotmail.com>
2024-05-21 11:55:14 -0300

    feat: basic calculation display add
    - missing some details: block div by 0, not allowing more than a two number evaluation, decimals handling

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function divide(a, b){
 };
 
 function operate(operator, firstNumber, secondNumber){
+    clearDisplay();
     switch(operator){
         case "+":
             return add(firstNumber, secondNumber);
@@ -31,32 +32,55 @@ function operate(operator, firstNumber, secondNumber){
     }
 };
 
-function addToDisplay(eventTarget){
-    displayDiv.innerHTML += eventTarget.innerHTML;
+function addToDisplay(text){
+    displayDiv.innerHTML += text;
     displayValue = displayDiv.innerHTML;
+};
+
+function organizeDisplayInfo(){
+    let numbers = displayValue.split(currentOperator);
+    console.log([currentOperator, numbers[0], numbers[1]])
+    return [currentOperator, numbers[0], numbers[1]];
 }
 
-
+function clearDisplay(){
+    displayDiv.innerHTML = "";
+    currentOperator = '';
+};
 
 /// GLOBALS ///////
 const operatorsList = ["+", "-", "x", "/"];
 const buttonsDiv = document.querySelector("#calculatorInput");
 const displayDiv = document.querySelector("#calculatorDisplay");
-const displayValue = "";
+let displayValue = "";
+let currentOperator = "";
 
 /// MAIN /////////
 buttonsDiv.addEventListener('click', (event) => {
     if(event.target.id === "calculatorInput") return; // prevent processing the parent of buttons
 
+    if(operatorsList.includes(event.target.innerHTML)){
+        currentOperator = event.target.innerHTML;
+    }
+
     switch(event.target.innerText){
         case "=":
-            operate();
+            let numbers, leftNumber, rightNumber;
+            numbers = organizeDisplayInfo().slice(1, 3);
+            leftNumber = numbers[0];
+            rightNumber = numbers[1];
+            
+            // TODO: make logic 7+8- cases
+            // TODO: block div by 0
+            if(leftNumber && rightNumber){
+                addToDisplay(operate(currentOperator, leftNumber, rightNumber));
+            }
             break;
         case "C":
             clearDisplay();
             break;
         default:
-            addToDisplay(event.target);
+            addToDisplay(event.target.innerHTML);
             break;
     };
 })

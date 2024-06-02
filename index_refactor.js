9a1660729c36c4f545ecd8c03e393b79c605eb94
FShinoda <fernanda.shinoda@hotmail.com>
2024-06-02 02:20:29 -0300

    refactor: input entering, operators input restrictions

--- index_refactor.js
+++ index_refactor.js
@@ -6,23 +6,83 @@ function subtract(a, b){
     return (+a)-(+b);
 };
 function multiply(a, b){
+    console.log((+a))
     return (+a)*(+b);
 };
 function divide(a, b){
     return (+a)/(+b);
 };
 
-function operate(){};
+function operate(){
+    let operation, leftNumber, rightNumber, total;
 
-function addNumber(){};
+    operation = displayValue.split(currentOperator);
+    leftNumber = operation[0];
+    rightNumber = operation[1];
+    console.log(leftNumber, rightNumber, currentOperator)
 
-function addOperator(){};
+    switch(currentOperator){
+        case "+":
+            total = add(leftNumber, rightNumber);
+            break;
+        case "-":
+            total = subtract(leftNumber, rightNumber);
+            break;
+        case "x":
+            total = multiply(leftNumber, rightNumber);
+            break;
+        case "/":
+            total = divide(leftNumber, rightNumber);
+            break;
+        default:
+            total = "probably an error";
+    }
 
-function addDecimalDot(){};
+    changeDisplay(total);
+};
+
+function addNumber(number){
+    if(displayValue == '0'){
+        changeDisplay(number, append=false);
+    } else {
+        changeDisplay(number, append=true);
+    }
+};
+
+function addOperator(operator){
+    if(operatorsList.includes(displayValue.slice(-1))){
+        changeDisplay(displayValue.slice(0, -1));
+    };
+    changeDisplay(operator, append=true);
+    currentOperator = operator;
+};
+
+function addDecimalDot(decimalDot){
+    if(displayValue.slice(-1) == '.'){
+        changeDisplay(displayValue.slice(0, -1));
+    };
+    changeDisplay(decimalDot, append=true);
+};
 
 function clearLast(){};
 
-function clearAll(){};
+function clearAll(){
+    changeDisplay('');
+};
+
+function resetDisplay(){
+    changeDisplay('0');
+};
+
+function changeDisplay(newValue, append=false){
+    if(append){
+        displayValue += newValue + "";
+        displayDiv.innerHTML += newValue + "";
+    } else {
+        displayValue = newValue + "";
+        displayDiv.innerHTML = newValue + "";
+    }
+};
 
 
 /// GLOBALS ------------------
@@ -30,33 +90,35 @@ const operatorsList = ["+", "-", "x", "/"]
     , buttonsDiv = document.querySelector("#calculatorInput")
     , displayDiv = document.querySelector("#calculatorDisplay");
 
-let displayValue = 0,
+let displayValue = '0',
     currentOperator = '';
 
 
 /// MAIN ------------------
 buttonsDiv.addEventListener('click', (event) => {
+    let newInput = event.target.innerText;
+
     if(event.target.id === "calculatorInput") return; // prevent processing the parent of buttons
-    if(displayValue == "!DIV" && event.target.innerText !== "C") return; // lock inputing when calculator display !DIV
+    if(displayValue == "!DIV" && newInput !== "C") return; // lock inputing when calculator display !DIV
 
     switch(event.target.innerText){
         case "=":
             operate();
             break;
         case "C":
-            clearAll();
+            resetDisplay();
             break;
         case "+":
         case "-":
         case "/":
         case "x":
-            addOperator();
+            addOperator(newInput);
             break;
         case ".":
-            addDecimalDot();
+            addDecimalDot(newInput);
             break;
         default:
-            addNumber();
+            addNumber(newInput);
             break;
     };
 
